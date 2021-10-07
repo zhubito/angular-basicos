293c338989c6c4eb647fe827789ce4efa9bd4356
Nicolas <zhubito@live.cl>
2021-10-06 23:42:58 -0300

    master

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+
+  nuevo: Personaje = {
+    nombre: 'roshi',
+    poder: 1000
+  }
+  
+  constructor() {
+  }
+  
+}
